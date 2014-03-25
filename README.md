47904ec10f644d70cb1b655754f7da608166ea71
Mike Sokolsky <mike@udacity.com>
2014-03-25 10:53:22 +0100

    Removed prob.py, edited README, added some comments, that sort of thing

--- README.md
+++ README.md
@@ -1,283 +1,42 @@
-# Brainjar.org - Battleships 
+# ayn-random bot
 
-![alt text](https://raw.github.com/BrainJar/battleships/master/resources/brainjar_org_logo_200.png "Logo Brainjar.org")
+A bot in python for the [Brainjar.org Battleships competition](https://www.brainjar.org/battleships/).
 
-This is the game engine for the Battleships coding challenge at [https://brainjar.org/battleships](https://brainjar.org/battleships)
+## Basics
 
-## Bots
+Plays a slightly smaller version of the classic game battleship, assuming the API proposed for the brainjar.org competition.
 
-This repos provides basic info about the game. If you want to see an example of a working bot to get you started, check this out: https://github.com/BrainJar/battleships-bot
 
+## Origins
 
-You will need a working local environment - check out https://github.com/BrainJar/battleships-bot#4-test-it-locally-before-the-fight
+This code is forked from [https://github.com/BrainJar/battleships-bot](https://github.com/BrainJar/battleships-bot)
 
-Remember that you lose each fight your bot crashes in !
 
+## LICENCES
 
-## Rules of the game
+### bot.py
 
+The MIT License (MIT)
 
-**The goal is to destroy the enemy's fleet.**
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 
-The game is played by two players. 
-
-Each player has a fleet of 4 ships of different sizes (2, 3, 4, 5) on a grid 8 x 8 (columns 0-7, rows 0-7).
-
-
-
-Players play in turns: in each turn they choose a cell to atack.
-
-Players can't see each other's fleet - they only know what is on the cells they have shot at.
-
-
-### Initial setting
-
-At the beginning, each player is choosing their setting - they have to place 4 ships on the grid.
-
-Player A:
-
-        0   1   2   3   4   5   6   7
-    0
-    1           X   X   X   X
-    2   X
-    3   X                   X
-    4                       X
-    5       X   X   X       X
-    6                       X
-    7                       X
-
-Player B:
-
-        0   1   2   3   4   5   6   7
-    0           X   X   X   X   X
-    1
-    2   X
-    3   X
-    4   X
-    5       X   X   X   X
-    6                       X
-    7                       X
-
-
-** Moves are stored as a couple (column, row), i.e. (2,1) is the beginning of size 4 ship of player A**
-
-### Turns
-
-In each turn player has to choose one location to shoot at. 
-If the opponnent doesn't have a ship at this location, the field is marked as "missed", and the turn ends.
-If the opponnent has a ship at this location, the field is marked as "hit", and the turn continues - player chooses another location.
-
-#### Pseudocode
-
-    game = new game()
-
-    game.setup(0, new Player().config)
-    game.setup(1, new Player().config)
-
-    player = game.randomPlayer()
-
-    while (!game.over()){
-
-        turn = player.play(game.snapshot)
-        if (game.isValid(turn)){
-            game.apply(turn)
-            if (game.missed){
-                player = game.nextPlayer()
-            }
-        } else{
-            game.lost(player)
-        }
-    }
-
-    return game.winner
-
-
-## Playing the game
-
-
-### Stateless, baby
-
-All bots have to be stateless. That's why for each move, you'll get complete game information.
-
-
-### Standard input and output
-
-All communication is via command line argument and stdout, via JSON. 
-
-We'll provide you with a valid json string as an argument, and we'll expect a valid JSON string as the only thing printed to the screen.
-
-As a consequence, any errors will be considered a suicide.
-
-
-
-### Beginning of the game
-
-#### Request for the init config
-
-The bot will receive a following JSON object.
-
-    {
-        "cmd": "init"
-    }
-
-#### Initial config format
-
-Initial config response has to follow this JSON format:
-
-    {
-        "2" :
-            {
-                "point": "00",
-                "orientation" : "vertical"   // possible values "horizontal", "vertical"
-            },
-        "3" :
-            {
-                "point": "22",
-                "orientation" : "vertical"
-            },
-        "4" :
-            {
-                "point": "42",
-                "orientation" : "vertical"
-            },
-        "5" :
-            {
-                "point": "37",
-                "orientation" : "horizontal"
-            }
-    }
-
-This initial config will represent
-
-
-        0   1   2   3   4   5   6   7
-    0   X
-    1   X
-    2           X       X
-    3           X       X
-    4           X       X
-    5                   X           
-    6
-    7               X   X   X   X   X
-
-
-### Moves
-
-#### Grid snapshots
-
-Before each move, player get the current situation - the opponent's grid's snapshot with marked fields.
-
-A snapshot is represented by a JSON:
-
-    {
-        "cmd": "move",              // for less user-friendly languages
-
-        // an array representing the sequence of moves and results (see below)
-        "moves": ["0001", "1003", "1113", ...],
-
-        // for your convenience, we also suply the following data
-        "hit"       : ["20", "30"],  // the cells shot at and hit
-        "missed"    : ["44", "01"],  // the cells shot at but missed
-        "destroyed" : [2],           // sizes (2, 3, 4, 5) of destroyed opponent's ships
-        "you"       : 0              // player 0 or 1
-    }
-
-In the array representing the sequence, (player, move, results) are encoded.
-
-    player = 0|1 // player number 0 or 1
-    move   = XY  // see below
-    result = 1|3 // 1 means missed, 3 means hit
-
-For example an array:
-
-    ["0001", "1003", "1113", "1331", "0123", "0133"]
-
-Represents:
- - player 0 shoots 00 and misses
- - player 1 shoots 00 and hits
- - player 1 shoots 11 and hits
- - player 1 shoots 33 and misses
- - player 0 shoots 12 and hits
- - player 0 shoots 13 and hits
-
-
-#### Move format
-
-A move (as returned by the bot) is represented by a string (column, row).
-
-A following JSON has to be returned for a bot to play a move.
-
-    {
-        "move" : "00"
-        // any invalid output or shooting twice at the same cell will be taken as a surrender
-    }
-
-
-
-#### Valid turn
-
-In a turn, player has to chose a valid location on the opponents side. Otherwise they lose.
-
-
-## Ranking - Scores
-
-When the game starts, each player has a score (initial score = 10, minimal score = 10).
-
-**Winner gets 5% of points of the looser.**
-
-**Looser loses 5% of their points.**
-
-
-## Getting started - tester.coffee
-
-There is a little tester program, which lets you verify that the things is going ok.
-
-    git clone git@github.com:BrainJar/battleships.git
-    cd battleships
-    npm install
-    # if you don't have coffeescript installed
-    npm install -g coffee-script
-    coffee tester.coffee
-
-What you get is a loop, asking for a valid move and showing all useful data.
-
-    =====================
-     Player 1 moves
-
-    prompt: move:  02
-    Snapshot: "{\"hit\":[\"00\",\"01\"],\"missed\":[\"02\"],\"destroyed\":[\"2\"],\"moves\":[\"1003\",\"1013\",\"1021\"]}"
-    (8,8), destroyed: 2
-    #                 
-    #                 
-    *   H   H         
-        H   H         
-        H   H         
-            H         
-                      
-          H H H H H   
-                      
-    {"hit":["00","01"],"missed":["02"],"destroyed":["2"]}
-    (8,8), destroyed: 
-    H                 
-    H                 
-        H   H         
-        H   H         
-        H   H         
-            H         
-                      
-          H H H H H   
-                      
-    {"hit":[],"missed":[],"destroyed":[]}
-
-
-## Status
-
-Beta. Most of the things are hacked together in few evening. Contributions welcome !
-
-
-## LICENCE
+### All files except for bot.py
 
 The MIT License (MIT)
 
--- bot.py
+++ bot.py
@@ -1,11 +1,241 @@
+'''
+Bot for www.brainjar.org/battleships/ competition.
+
+Originally authored by github.com/mdevs5531
+'''
 import json
 import random
 import sys
 
-import prob
+
+def str2tuple(string_point):
+    return tuple(map(int, string_point))
+
+
+def tuple2str(tuple_point):
+    return "".join(map(str, tuple_point))
+
+
+class LikelyhoodBot(object):
+    '''
+    Not perfect by any means, but figure if I can't beat it more than 50%
+    of the time it's not bad.
+    '''
+
+    # Potential states for each square on the board
+    UNKNOWN = 0
+    HIT = 1
+    MISS = 2
+    SUNK = 3
+
+    def __init__(self, state):
+        # Seed your random number generator!
+        random.seed()
+        # State of each square on the board
+        self.state = {}
+        # Holds a non-bounded number for each square, higher numbers
+        # mean it's a more appealing target
+        self.fits = {}
+        # Which ships still need to be sunk?
+        self.remaining_ships = [5, 4, 3, 2]
+        for ship in state['destroyed']:
+            self.remaining_ships.remove(int(ship))
+        # Initialize state and fits
+        for x in range(8):
+            for y in range(8):
+                point = (x, y)
+                self.state[point] = self.UNKNOWN
+                self.fits[point] = 0
+        # me = you
+        me = str(state['you'])
+        # Run through all moves and update the state of the board
+        for move in state['moves']:
+            if move[0:1] != me:
+                continue
+            point = str2tuple(move[1:3])
+            result = move[3:4]
+            if result == '1':
+                self.miss(point)
+            elif result == '3':
+                self.hit(point)
+            elif result == '4':
+                self.sunk(point, int(state['destroyed'].pop(0)))
+        self.calculateFits()
+        self.calculateAdjacents()
+
+    def getMaxMove(self):
+        '''
+        Returns the square (in 'xy' format) that has the highest fit value
+        '''
+        items = self.fits.items()
+        if len(items) == 0:
+            return None
+        max_value = max(items, key=lambda x: x[1])
+        move = filter(lambda x: x[1] == max_value[1], items)[0][0]
+        return tuple2str(move)
+
+    def printState(self):
+        '''
+        Easy to look at visulaization of what the bot's belief in the
+        board state is
+        '''
+        for y in range(8):
+            for x in range(8):
+                if self.state[(x, y)] == self.HIT:
+                    print ' #  ',
+                elif self.state[(x, y)] == self.MISS:
+                    print ' *  ',
+                elif self.state[(x, y)] == self.SUNK:
+                    print '### ',
+                else:
+                    print '%3d ' % self.fits[(x, y)],
+            print ''
+
+    def doesShipFit(self, length, point, direction):
+        '''
+        Checks if it's possible for an unsunk ship of _length_ to be
+        on the board, starting at _point_ (as a minimum coordinate) and
+        extending in _direction_
+
+        Code is kinda ugly
+        '''
+        fits = True
+        if direction == 'horizontal':
+            y = point[1]
+            for x in range(point[0], point[0] + length):
+                state = self.state[(x, y)]
+                if state == self.MISS or state == self.SUNK:
+                    fits = False
+                    break
+        else:
+            x = point[0]
+            for y in range(point[1], point[1] + length):
+                state = self.state[(x, y)]
+                if state == self.MISS or state == self.SUNK:
+                    fits = False
+                    break
+        return fits
+
+    def calculateFits(self):
+        '''
+        Check all possible positions for all possible remaining ships to see
+        if they are valid.  Increments fits by 1 for each possible ship +
+        position + direction combo that could be there.
+
+        Caveat: Assumes independence of ship positions, which is untrue.
+        However in practice this is a close enough approximation, and means
+        we only have to test 352 positions rather than some awful combinatorial
+        problem.
+
+        Caveat 2: Assumes ships are more likely to be located in the center
+        of the board because there are more possible positions for them there.
+        This holds some validity against random ship placement of the kind this
+        bot uses, but is pretty shaky math and I haven't really thought it
+        through.
+        '''
+        for length in self.remaining_ships:
+            # Verical checks
+            for x in range(0, 8):
+                for y in range(0, 9 - length):
+                    point = (x, y)
+                    fit = int(self.doesShipFit(length, point, 'vertical'))
+                    for y_fit in range(y, y + length):
+                        fit_point = (x, y_fit)
+                        if fit_point in self.fits:
+                            self.fits[fit_point] += fit
+            # Horizontal checks
+            for y in range(0, 8):
+                for x in range(0, 9 - length):
+                    point = (x, y)
+                    fit = int(self.doesShipFit(length, point, 'horizontal'))
+                    for x_fit in range(x, x + length):
+                        fit_point = (x_fit, y)
+                        if fit_point in self.fits:
+                            self.fits[fit_point] += fit
+
+    def calculateAdjacents(self):
+        '''
+        Look through each possible remaining shot, and add some value
+        depending on if there are adjacent HITs on non-SUNK ships.
+
+        The + 30 for each consecutive adjacent HIT is arbitrary, but
+        higher than the maximum fits score possible in the initial board.
+        '''
+        def check(point, direction):
+            new_point = (point[0] + direction[0], point[1] + direction[1])
+            if new_point in self.state:
+                state = self.state[new_point]
+                if state == self.MISS or state == self.SUNK:
+                    return 0
+                elif state == self.HIT:
+                    return 30 + check(new_point, direction)
+            return 0
+
+        directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        for x in range(0, 8):
+            for y in range(0, 8):
+                point = (x, y)
+                if point in self.fits:
+                    for direction in directions:
+                        self.fits[point] += check(point, direction)
+
+    def hit(self, point):
+        '''
+        Mark square as hit
+        '''
+        self.state[point] = self.HIT
+        del self.fits[point]
+
+    def miss(self, point):
+        '''
+        Mark square as missed
+        '''
+        self.state[point] = self.MISS
+        del self.fits[point]
+
+    def sunk(self, point, length):
+        '''
+        Mark square as sunk, and try to figure out which adjacent squares
+        represent the rest of the sunk ship.  This should work in the nice
+        clean world we play in right now, where people seed ships that are
+        strictly non-adjacent.  However, if ships can be placed adjacent to
+        each other it's easily possible for this to mark the wrong squares.
+        '''
+        x, y = point
+        possible_size = 1
+        while x - possible_size >= 0 and self.state[(x - possible_size, y)] == self.HIT:
+            possible_size += 1
+        if possible_size >= length:
+            for x in range(x - length + 1, x + 1):
+                self.state[(x, y)] = self.SUNK
+            return
+        possible_size = 1
+        while x + possible_size <= 7 and self.state[(x + possible_size, y)] == self.HIT:
+            possible_size += 1
+        if possible_size >= length:
+            for x in range(x, x + length):
+                self.state[(x, y)] = self.SUNK
+            return
+        possible_size = 1
+        while y - possible_size >= 0 and self.state[(x, y - possible_size)] == self.HIT:
+            possible_size += 1
+        if possible_size >= length:
+            for y in range(y - length + 1, y + 1):
+                self.state[(x, y)] = self.SUNK
+            return
+        possible_size = 1
+        while y + possible_size <= 7 and self.state[(x, y + possible_size)] == self.HIT:
+            possible_size += 1
+        if possible_size >= length:
+            for y in range(y, y + length):
+                self.state[(x, y)] = self.SUNK
+            return
 
 
 class Placement(object):
+    '''
+    Pretty dumb code but it works.  Not really worth commenting.
+    '''
 
     def __init__(self):
         self.state = [[0 for y in range(8)] for x in range(8)]
@@ -78,138 +308,13 @@ class Placement(object):
                                        'orientation': direction}
 
 
-MISS = -10
-HIT = -5
-
-
-class Bot(object):
-
-    def __init__(self, ai='smarter_bot'):
-        self.play = getattr(self, ai)
-        random.seed()
-        #self.log = open('play.log', 'a')
-
-    def construct_state(self, state):
-        self.state = [[0 for y in range(8)] for x in range(8)]
-        self.free = []
-        for x in range(8):
-            for y in range(8):
-                self.free.append((x, y))
-        for move in state['hit']:
-            move = tuple(map(int, move))
-            y, x = move
-            self.state[x][y] = HIT
-            self.free.remove(move)
-        for move in state['missed']:
-            move = tuple(map(int, move))
-            y, x = move
-            self.state[x][y] = MISS
-            self.free.remove(move)
-
-    def mark_ships_naieve(self):
-        for x in range(8):
-            for y in range(8):
-                if self.state[x][y] == HIT:
-                    bounds = self.get_ship_bounds(x, y)
-                    if bounds['x_min'] > 0:
-                        self.state[bounds['x_min'] - 1][y] += 0.5
-                    if bounds['x_max'] < 7:
-                        self.state[bounds['x_max'] + 1][y] += 0.5
-                    if bounds['y_min'] > 0:
-                        self.state[x][bounds['y_min'] - 1] += 0.5
-                    if bounds['y_max'] < 7:
-                        self.state[x][bounds['y_max'] + 1] += 0.5
-
-    def get_ship_bounds(self, x, y):
-        # test X:
-        x_min = x
-        x_max = x
-        x_bounded = True
-        for x_off in reversed(range(0, x)):
-            if self.state[x_off][y] == HIT:
-                x_min = x_off
-            elif self.state[x_off][y] < 0:
-                break
-            else:
-                x_bounded = False
-                break
-        for x_off in range(x + 1, 8):
-            if self.state[x_off][y] == HIT:
-                x_max = x_off
-            elif self.state[x_off][y] < 0:
-                break
-            else:
-                x_bounded = False
-                break
-        res = {'x_min': x_min, 'x_max': x_max, 'x_bounded': x_bounded}
-        y_min = y
-        y_max = y
-        y_bounded = True
-        for y_off in reversed(range(0, y)):
-            if self.state[x][y_off] == HIT:
-                y_min = y_off
-            elif self.state[x][y_off] < 0:
-                break
-            else:
-                y_bounded = False
-                break
-        for y_off in range(y + 1, 8):
-            if self.state[x][y_off] == HIT:
-                y_max = y_off
-            elif self.state[x][y_off] < 0:
-                break
-            else:
-                y_bounded = False
-                break
-        res.update({'y_min': y_min, 'y_max': y_max, 'y_bounded': y_bounded})
-        return res
-
-    def random_move(self):
-        move = self.free.pop(random.randint(0, len(self.free) - 1))
-        return "".join(map(str, move))
-
-    def max_value(self):
-        maximum = 0
-        for row in range(0, 8):
-            if max(self.state[row]) > maximum:
-                maximum = max(self.state[row])
-        return maximum
-
-    def max_move(self):
-        max_row = 0
-        maximum = 0
-        for row in range(0, 8):
-            if max(self.state[row]) > maximum:
-                max_row = row
-                maximum = max(self.state[row])
-        max_col = self.state[max_row].index(max(self.state[max_row]))
-        return "".join(map(str, (max_col, max_row)))
-
-    def random_bot(self, state):
-        self.construct_state(state)
-        move = {'move': self.random_move()}
-        print json.dumps(move)
-
-    def dumb_bot(self, state):
-        self.construct_state(state)
-        self.mark_ships_naieve()
-        #self.log.write(str(self.state) + '\n')
-        #self.log.flush()
-        move = {'move': self.random_move()}
-        if self.max_value() > 0:
-            move = {'move': self.max_move()}
-        print json.dumps(move)
-
-    def smarter_bot(self, state):
-        bot = prob.Probability(state)
-        move = {'move': bot.getMaxMove()}
-        print json.dumps(move)
-
 if __name__ == "__main__":
+    # Get input command
     state = json.loads(sys.argv[1])
     if state['cmd'] == 'init':
         place = Placement()
         place.place_ships()
     else:
-        bot = Bot()
-        bot.play(state)
+        bot = LikelyhoodBot(state)
+        move = {'move': bot.getMaxMove()}
+        print json.dumps(move)
